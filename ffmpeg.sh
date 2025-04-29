31a99cfe13c5b0c300831242f0b82849f18b7428
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2025-04-29 23:42:55 +0900

    修正

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-input_file="/mnt/kiso-qnap3/hurigana-speech-corpus-aozora/test/4SBmtE4oT0/Boqfggcfos/yOr4eeSK8I/QXPS9XBZnsZYReQqe1uV.mp3"
-output_file="/mnt/kiso-qnap3/yuabe/m1/useWhisper/data/audio/short_clip.mp3"
+input_file="/mnt/kiso-qnap3/hurigana-speech-corpus-aozora/train/1y4eRgkEey/3VA5QzZL1f/D50JsbGwQA/9Y1hiCvbzIPfqYooLoME.mp3"
+output_file="/mnt/kiso-qnap3/yuabe/m1/useWhisper/data/train/audio/short_clip.mp3"
 
 ffmpeg -y -i "$input_file" -t 30 "$output_file"
