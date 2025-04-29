2b04d09f40fcb4e79bfa98dcd54ea74afd8dcb89
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2025-04-29 23:58:03 +0900

    サンプルデータで確認

--- oss_sample.py
+++ oss_sample.py
@@ -1,7 +1,9 @@
 import time
 import whisper
 
-input_file = "/mnt/kiso-qnap3/yuabe/m1/useWhisper/data/short_clip.mp3"  # 音声データのファイルパス
+input_file = "/mnt/kiso-qnap3/yuabe/m1/useWhisper/data/train/audio/short_clip.mp3"  # 音声データのファイルパス
+output_file = "/mnt/kiso-qnap3/yuabe/m1/useWhisper/data/train/text/transcription.txt"  # 出力ファイル
+
 model = whisper.load_model("medium")  # 使用するモデルを指定する
 
 start_time = time.time()
@@ -9,3 +11,7 @@ result = model.transcribe(input_file, fp16=False)  # 音声データの文字起
 end_time = time.time()
 print(result["text"])  # 文字起こし結果の表示
 print(f"実行時間: {end_time - start_time} seconds")
+
+# 結果をファイルに保存
+with open(output_file, "w", encoding="utf-8") as f:
+    f.write(result["text"] + "\n")
